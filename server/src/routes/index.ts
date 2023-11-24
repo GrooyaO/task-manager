045d15de6c989fce12b6a874f3a3c1bbe814954a
Stevan <grujic.s96cf@gmail.com>
2023-11-24 16:24:15 +0100

    feat: delete task server funcionality added

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 
 export default function defineRoutes(app: express.Application) {
   const router = express.Router()
+
   //GET - retrieve tasks
   router.get('/tasks', async (req: Request, res: Response) => {
     const tasks = await Task.find()
@@ -27,6 +28,22 @@ export default function defineRoutes(app: express.Application) {
           .json({ message: 'Validation Error', details: error.message })
       }
       console.error('Unexpected error:', error)
+      res.status(500).json({ message: 'Internal Server Error' })
+    }
+  })
+
+  // DELETE - delete task
+  router.delete('/task/:id', async (req: Request, res: Response) => {
+    try {
+      const deletedTask = await Task.findByIdAndDelete(req.params.id)
+      if (!deletedTask) {
+        return res.status(404).json({ message: 'Task not found' })
+      }
+      res
+        .status(200)
+        .json({ success: true, message: 'Task deleted successfully' })
+    } catch (error) {
+      console.error('Error deleting task:', error)
       res.status(500).json({ message: 'Internal Server Error' })
     }
   })
